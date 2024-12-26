1fee66fc5267a5daf4f073b751b4177222de41a4
Rai <151483030+Rai1990@users.noreply.github.com>
2024-12-26 18:58:10 +0900

    Update ai-chat-shadow.js

--- ai-chat-shadow.js
+++ ai-chat-shadow.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 cursor: pointer;
                 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
                 z-index: 10001; /* 다른 요소 위에 표시 */
-                margin:30px;
+                margin:60px;
             }
 
             .chat-button img {
